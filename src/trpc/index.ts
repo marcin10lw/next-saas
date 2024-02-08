facd2770252b1c8456a76ca900701f46bf366299
marcin10lw <marcin10lw@wp.pl>
2024-02-08 16:36:26 +0100

    feat(messages): created getFileMessages route with infinite query cursor logic

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -4,6 +4,7 @@ import { UTApi } from "uploadthing/server";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import z from "zod";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -102,6 +103,57 @@ export const appRouter = router({
       return {
         status: file.uploadStatus,
       };
+    }),
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT;
+
+      const file = await db.file.findFirst({
+        where: {
+          userId,
+          id: fileId,
+        },
+      });
+
+      if (!file) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const messages = await db.message.findMany({
+        where: {
+          fileId: file.id,
+          userId,
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        take: limit + 1,
+        orderBy: {
+          createdAt: "desc",
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          isUserMessage: true,
+          text: true,
+        },
+      });
+
+      let nextCursor: string | undefined = undefined;
+
+      if (messages.length > limit) {
+        const lastMessage = messages.pop();
+        nextCursor = lastMessage?.id;
+      }
+
+      return { messages, nextCursor };
     }),
 });
 
