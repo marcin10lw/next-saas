8c6ac687bc3ba3f8759e7faef29a6b274b77e977
marcin10lw <marcin10lw@wp.pl>
2024-02-14 22:32:50 +0100

    fix(auth-callback): moved client code to components

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,40 +1,7 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
-import { trpc } from "../_trpc/client";
-import { useEffect } from "react";
-import { ROUTES } from "@/common/navigation/routes";
-import { Loader2 } from "lucide-react";
+import AuthCallback from "@/components/AuthCallback";
 
 const Page = () => {
-  const router = useRouter();
-
-  const searchParams = useSearchParams();
-  const origin = searchParams.get("origin");
-
-  const { data, error } = trpc.authCallback.useQuery(undefined, {
-    retry: false,
-  });
-
-  useEffect(() => {
-    if (data?.success) {
-      router.push(origin ? `/${origin}` : ROUTES.dashboard);
-    }
-
-    if (error?.data?.code === "UNAUTHORIZED") {
-      router.push("/sign-in");
-    }
-  }, [data, error, origin, router]);
-
-  return (
-    <div className="mt-24 flex w-full justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="text-xl font-semibold">Setting up your account...</h3>
-        <p>You will be redirected automatically.</p>
-      </div>
-    </div>
-  );
+  return <AuthCallback />;
 };
 
 export default Page;
---
+++ src/components/AuthCallback.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { ROUTES } from "@/common/navigation/routes";
+import { trpc } from "@/app/_trpc/client";
+import { Loader2 } from "lucide-react";
+
+const AuthCallback = () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const { data, error } = trpc.authCallback.useQuery(undefined, {
+    retry: false,
+  });
+
+  useEffect(() => {
+    if (data?.success) {
+      router.push(origin ? `/${origin}` : ROUTES.dashboard);
+    }
+
+    if (error?.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
+    }
+  }, [data, error, origin, router]);
+
+  return (
+    <div className="mt-24 flex w-full justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="text-xl font-semibold">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthCallback;
